c248ade97d02a29db554a4ecf24da36993b70244
a.makihara <a.makihara@auris.jp>
2023-07-13 10:28:58 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [page, setPage] = useState('home');
+
+  const startHandler = () => {
+    setPage('upload');
+  }
+
+  const rankingHandler = () => {
+    setPage('ranking');
+  }
+
+  if (page === 'home') {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          顔面強さランキング
+        </Typography>
+        <Button variant="contained" onClick={startHandler}>START</Button>
+        <Button variant="contained" onClick={rankingHandler}>RANKING</Button>
+      </Box>
+    );
+  }
+  // 他のページ (upload, ranking) の表示をここに追加...
 }
 
 export default App;
